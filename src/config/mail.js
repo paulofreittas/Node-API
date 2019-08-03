7f794dc848900d710933c307998c7f445fb8db9b
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2019-08-03 01:26:13 -0300

    Cancellation feature implemented with email sending using nodemailer.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '52d0ff7ae6e0bd',
+    pass: '25eb1c1a4ab15a',
+  },
+  default: {
+    from: 'Team GoBarber <noreply@gobarber.com>',
+  },
+};
